84fcdc73a4d19f8b37b31158c5543a9dde5888e6
Nabeel Rehemtulla <nabeelre@gmail.com>
2025-05-06 13:11:39 -0500

    allow embedding image in larger figure

--- utils.py
+++ utils.py
@@ -3,13 +3,17 @@ from PIL import Image
 import os
 
 
-def show_image_by_assetid(asset_id, images_dir="data/3565489/images/"):
+def show_image_by_assetid(asset_id, images_dir="data/3565489/images/", ax=None):
     file_path = os.path.join(images_dir, f"{asset_id}.jpg")
     if os.path.exists(file_path):
         img = Image.open(file_path)
 
-        plt.imshow(img)
-        plt.axis('off')
-        plt.show()
+        if ax is not None:
+            ax.imshow(img)
+            ax.axis('off')
+        else:
+            plt.imshow(img)
+            plt.axis('off')
+            plt.show()
     else:
         print(f"File not found: {file_path}")
